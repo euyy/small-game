3898b8236354f88bcc3c18b103152d8d656d13e7
昊 张 <gcrth@outlook.com>
2019-06-02 21:12:46 +0800

    basic

--- small game/sou.asm
+++ small game/sou.asm
@@ -11,11 +11,48 @@ include		include/kernel32.inc
 includelib	kernel32.lib
 include		include/msvcrt.inc	
 includelib	msvcrt.lib
+include		include/Gdi32.inc
+includelib	Gdi32.lib
 
 sprintf		proto	C :dword, :dword, :vararg
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; Equ 等值定义
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CLOCK_SIZE	equ	600
+ICO_MAIN	equ	100
+IDC_MAIN	equ	100
+IDC_MOVE	equ	101
+IDB_BACK1	equ	100
+IDB_CIRCLE1	equ	101
+IDB_MASK1	equ	102
+IDB_BACK2	equ	103
+IDB_CIRCLE2	equ	104
+IDB_MASK2	equ	105
+ID_TIMER	equ	1
+IDM_BACK1	equ	100
+IDM_BACK2	equ	101
+IDM_CIRCLE1	equ	102
+IDM_CIRCLE2	equ	103
+IDM_EXIT	equ	104
+
+
+BLOCK_SIZE	equ 30
+BLOCK_NUM_X equ 15
+BLOCK_NUM_Y equ 20
+WIN_SIZE_X	equ 15*30;BLOCK_NUM_X*BLOCK_SIZE
+WIN_SIZE_Y	equ 20*30;BLOCK_NUM_Y*BLOCK_SIZE
+
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 数据结构
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+Tetris struct
+kind		db	?
+direction	db	?
+centerPosX	dd	?
+centerPosY	dd	?
+Tetris ends	
+
 
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 数据段
@@ -23,18 +60,476 @@ sprintf		proto	C :dword, :dword, :vararg
 		.data
 hInstance	dd		?
 hWinMain	dd		?
+hCursorMove	dd		?	;Cursor when move
+hCursorMain	dd		?	;Cursor when normal
+hMenu		dd		?
+
+hBmpBack	dd		?
+hDcBack		dd		?
+hBmpClock	dd		?
+hDcClock	dd		?
+
+dwNowBack	dd		?
+dwNowCircle	dd		?
+
+lastTime	SYSTEMTIME <>
+
+blockList	db 1000 DUP(0)
+
+te		Tetris <>
+
+		.const
+
+szClassName	db	'Clock',0
+dwPara180	dw	180
+dwRadius	dw	CLOCK_SIZE/2
+szMenuBack1	db	'使用格子背景(&A)',0
+szMenuBack2	db	'使用花布背景(&B)',0
+szMenuCircle1	db	'使用淡蓝色边框(&C)',0
+szMenuCircle2	db	'使用粉红色边框(&D)',0
+szMenuExit	db	'退出(&X)...',0
 
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 代码段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		.code
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_CreateFrontPic	proc
+		local	@stTime:SYSTEMTIME
+		local	idx,posX,posY,posX_,posY_
+		pushad
+		invoke	BitBlt,hDcClock,0,0,WIN_SIZE_X,WIN_SIZE_Y,hDcBack,0,0,SRCCOPY
+;********************************************************************
+; 画时钟指针
+;********************************************************************
+		invoke	GetLocalTime,addr @stTime
+		;invoke time
+		movzx	eax,@stTime.wSecond
+		mov	ecx,360/60
+		mul	ecx			;秒针度数 = 秒 * 360/60
+;********************************************************************
+		invoke  CreateSolidBrush,76EE00h
+		invoke	SelectObject,hDcClock,eax
+		invoke	DeleteObject,eax
+		invoke	CreatePen,PS_SOLID,3,0
+		invoke	SelectObject,hDcClock,eax
+		invoke	DeleteObject,eax
+		;invoke	Rectangle,hDcClock,50,50,80,80
 
+		.if te.kind==0
+			.if te.direction==0
+				mov eax,te.centerPosX
+				mov ebx,te.centerPosY
+				;sub eax,BLOCK_SIZE
+				sub ebx,BLOCK_SIZE
+				mov posX,eax
+				mov posY,ebx
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				;add posX,BLOCK_SIZE
+				add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				;add posX,BLOCK_SIZE
+				add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				;add posX,BLOCK_SIZE
+				add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+			.elseif	te.direction==1
+				mov eax,te.centerPosX
+				mov ebx,te.centerPosY
+				sub eax,BLOCK_SIZE
+				;sub ebx,BLOCK_SIZE
+				mov posX,eax
+				mov posY,ebx
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				add posX,BLOCK_SIZE
+				;add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				add posX,BLOCK_SIZE
+				;add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+				add posX,BLOCK_SIZE
+				;add posY,BLOCK_SIZE
+				mov eax,posX
+				mov ebx,posY
+				add eax,BLOCK_SIZE
+				mov posX_,eax
+				sub ebx,BLOCK_SIZE
+				mov posY_,ebx
+				invoke	Rectangle,hDcClock,posX,posY,posX_,posY_
+			.endif	
+		.endif
+
+;********************************************************************
+		invoke	GetStockObject,NULL_PEN
+		invoke	SelectObject,hDcClock,eax
+		invoke	DeleteObject,eax
+
+
+
+
+		popad
+		ret
+
+_CreateFrontPic	endp
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-start:
+_CreateBackGround	proc
+			local	@hDc,@hDcCircle,@hDcMask
+			local	@hBmpBack,@hBmpCircle,@hBmpMask
+			local	idx,posX,posY,posX_,posY_
+;********************************************************************
+; 建立需要的临时对象
+;********************************************************************
+		invoke	GetDC,hWinMain
+		mov	@hDc,eax
+		invoke	CreateCompatibleDC,@hDc
+		mov	hDcBack,eax
+		invoke	CreateCompatibleDC,@hDc
+		mov	hDcClock,eax
+		invoke	CreateCompatibleDC,@hDc
+		mov	@hDcCircle,eax
+		invoke	CreateCompatibleDC,@hDc
+		mov	@hDcMask,eax
+		invoke	CreateCompatibleBitmap,@hDc,WIN_SIZE_X,WIN_SIZE_Y
+		mov	hBmpBack,eax
+		invoke	CreateCompatibleBitmap,@hDc,WIN_SIZE_X,WIN_SIZE_Y
+		mov	hBmpClock,eax
+		invoke	ReleaseDC,hWinMain,@hDc
+
+		invoke	LoadBitmap,hInstance,dwNowBack
+		mov	@hBmpBack,eax
+		invoke	LoadBitmap,hInstance,dwNowCircle
+		mov	@hBmpCircle,eax
+		mov	eax,dwNowCircle
+		inc	eax
+		invoke	LoadBitmap,hInstance,eax
+		mov	@hBmpMask,eax
+
+		invoke	SelectObject,hDcBack,hBmpBack
+		invoke	SelectObject,hDcClock,hBmpClock
+		invoke	SelectObject,@hDcCircle,@hBmpCircle
+		invoke	SelectObject,@hDcMask,@hBmpMask
+;********************************************************************
+; 以背景图片填充
+;********************************************************************
+		invoke	CreatePatternBrush,@hBmpBack
+		push	eax
+		invoke	SelectObject,hDcBack,eax
+		invoke	PatBlt,hDcBack,0,0,WIN_SIZE_X,WIN_SIZE_Y,PATCOPY
+		pop	eax
+		invoke	DeleteObject,eax
+;********************************************************************
+; 画底层格子
+;********************************************************************
+		
+		invoke  CreateSolidBrush,76EE00h
+		invoke	SelectObject,hDcBack,eax
+		invoke	DeleteObject,eax
+		invoke	CreatePen,PS_SOLID,3,0
+		invoke	SelectObject,hDcBack,eax
+		invoke	DeleteObject,eax
+		
+		;mov blockList[1],1
+		;mov blockList[2],1
+
+		mov idx,0
+		.while idx<BLOCK_NUM_X*BLOCK_NUM_Y
+			mov eax,idx
+			.if blockList[eax]==1
+				xor edx,edx
+				mov ebx,BLOCK_NUM_X
+				div ebx
+
+				mov ebx,BLOCK_SIZE
+				push edx
+				inc eax
+				mul ebx
+				mov posY,eax
+				sub eax,ebx
+				mov posY_,eax
+
+				pop eax
+				mul ebx
+				mov posX,eax
+				add eax,ebx
+				mov posX_,eax
+				invoke	Rectangle,hDcBack,posX,posY,posX_,posY_
+			.endif	
+			inc idx
+		.endw	
+		
+
+
+
+
+
+		invoke	DeleteDC,@hDcCircle
+		invoke	DeleteDC,@hDcMask
+		invoke	DeleteObject,@hBmpBack
+		invoke	DeleteObject,@hBmpCircle
+		invoke	DeleteObject,@hBmpMask
+		ret
+
+_CreateBackGround	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_DeleteBackGround	proc
+
+		invoke	DeleteDC,hDcBack
+		invoke	DeleteDC,hDcClock
+		invoke	DeleteObject,hBmpBack
+		invoke	DeleteObject,hBmpClock
+		ret
+
+_DeleteBackGround	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_Init		proc
+		local	@hBmpBack,@hBmpCircle
+;********************************************************************
+; for debug
+;********************************************************************
+mov te.kind,0
+mov te.direction,0
+mov te.centerPosX,120
+mov te.centerPosY,120
+
+;********************************************************************
+; 初始化菜单
+;********************************************************************
+		invoke	CreatePopupMenu
+		mov	hMenu,eax
+		;invoke	AppendMenu,hMenu,0,IDM_BACK1,offset szMenuBack1
+		;invoke	AppendMenu,hMenu,0,IDM_BACK2,offset szMenuBack2
+		;invoke	AppendMenu,hMenu,MF_SEPARATOR,0,NULL
+		;invoke	AppendMenu,hMenu,0,IDM_CIRCLE1,offset szMenuCircle1
+		;invoke	AppendMenu,hMenu,0,IDM_CIRCLE2,offset szMenuCircle2
+		;invoke	AppendMenu,hMenu,MF_SEPARATOR,0,NULL
+		invoke	AppendMenu,hMenu,0,IDM_EXIT,offset szMenuExit
+		;invoke	CheckMenuRadioItem,hMenu,IDM_BACK1,IDM_BACK2,IDM_BACK1,NULL
+		;invoke	CheckMenuRadioItem,hMenu,IDM_CIRCLE1,IDM_CIRCLE2,IDM_CIRCLE1,NULL
+;********************************************************************
+; 设置圆形窗口并设置“总在最前面”
+;********************************************************************
+		invoke	SetWindowPos,hWinMain,HWND_TOPMOST,0,0,0,0,\
+			SWP_NOMOVE or SWP_NOSIZE
+;********************************************************************
+; 建立背景
+;********************************************************************
+		mov	dwNowBack,IDB_BACK1
+		mov	dwNowCircle,IDB_CIRCLE1
+		invoke	_CreateBackGround
+		invoke	_CreateFrontPic
+		invoke	SetTimer,hWinMain,ID_TIMER,100,NULL
+
+		ret
+
+_Init		endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_Quit		proc
+
+		invoke	KillTimer,hWinMain,ID_TIMER
+		invoke	DestroyWindow,hWinMain
+		invoke	PostQuitMessage,NULL
+		invoke	_DeleteBackGround
+		invoke	DestroyMenu,hMenu
+		ret
+
+_Quit		endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_ProcWinMain	proc	uses ebx edi esi hWnd,uMsg,wParam,lParam
+		local	@stPS:PAINTSTRUCT
+		local	@hDC
+		local	@stPos:POINT
+
+		mov	eax,uMsg
+;********************************************************************
+		.if	eax ==	WM_TIMER
+			invoke	_CreateFrontPic
+			invoke	InvalidateRect,hWnd,NULL,FALSE
+;********************************************************************
+		.elseif	eax ==	WM_PAINT
+			invoke	BeginPaint,hWnd,addr @stPS
+			mov	@hDC,eax
+
+			mov	eax,@stPS.rcPaint.right
+			sub	eax,@stPS.rcPaint.left
+			mov	ecx,@stPS.rcPaint.bottom
+			sub	ecx,@stPS.rcPaint.top
+
+			invoke	BitBlt,@hDC,@stPS.rcPaint.left,@stPS.rcPaint.top,eax,ecx,\
+				hDcClock,@stPS.rcPaint.left,@stPS.rcPaint.top,SRCCOPY
+			invoke	EndPaint,hWnd,addr @stPS
+;********************************************************************
+		.elseif	eax ==	WM_CREATE
+			mov	eax,hWnd
+			mov	hWinMain,eax
+			invoke	_Init
+;********************************************************************
+		.elseif	eax == WM_KEYDOWN
+			mov eax,wParam
+			.if eax == VK_Q
+				call	_Quit
+				xor	eax,eax
+				ret
+			.elseif	eax == VK_W;change direction
+				.if te.kind ==0
+					.if te.direction==0
+						mov te.direction,1
+					.elseif	 te.direction==1
+						mov te.direction,0
+					.endif	
+				.endif	
+				
+			.elseif	eax == VK_A
+
+			.elseif	eax == VK_S
+
+			.elseif	eax == VK_D
+
+			.endif	
+		.elseif	eax ==	WM_COMMAND
+			mov	eax,wParam
+			.if	ax ==	IDM_BACK1
+				mov	dwNowBack,IDB_BACK1
+				invoke	CheckMenuRadioItem,hMenu,IDM_BACK1,IDM_BACK2,IDM_BACK1,NULL
+			.elseif	ax ==	IDM_BACK2
+				mov	dwNowBack,IDB_BACK2
+				invoke	CheckMenuRadioItem,hMenu,IDM_BACK1,IDM_BACK2,IDM_BACK2,NULL
+			.elseif	ax ==	IDM_CIRCLE1
+				mov	dwNowCircle,IDB_CIRCLE1
+				invoke	CheckMenuRadioItem,hMenu,IDM_CIRCLE1,IDM_CIRCLE2,IDM_CIRCLE1,NULL
+			.elseif	ax ==	IDM_CIRCLE2
+				mov	dwNowCircle,IDB_CIRCLE2
+				invoke	CheckMenuRadioItem,hMenu,IDM_CIRCLE1,IDM_CIRCLE2,IDM_CIRCLE2,NULL
+			.elseif	ax ==	IDM_EXIT
+				call	_Quit
+				xor	eax,eax
+				ret
+			.endif
+			invoke	_DeleteBackGround
+			invoke	_CreateBackGround
+			invoke	_CreateFrontPic
+			invoke	InvalidateRect,hWnd,NULL,FALSE
+;********************************************************************
+		.elseif	eax ==	WM_CLOSE
+			call	_Quit
+;********************************************************************
+; 按下右键时弹出一个POPUP菜单
+;********************************************************************
+		.elseif eax == WM_RBUTTONDOWN
+			invoke	GetCursorPos,addr @stPos
+			invoke	TrackPopupMenu,hMenu,TPM_LEFTALIGN,@stPos.x,@stPos.y,NULL,hWnd,NULL
+;********************************************************************
+; 由于没有标题栏，下面代码用于按下左键时移动窗口
+; UpdateWindow：即时刷新，否则要等到放开鼠标时窗口才会重画
+;********************************************************************
+		.elseif eax ==	WM_LBUTTONDOWN
+			invoke	SetCursor,hCursorMove
+			invoke	UpdateWindow,hWnd
+			invoke	ReleaseCapture
+			invoke	SendMessage,hWnd,WM_NCLBUTTONDOWN,HTCAPTION,0
+			invoke	SetCursor,hCursorMain
+;********************************************************************
+		.else
+			invoke	DefWindowProc,hWnd,uMsg,wParam,lParam
+			ret
+		.endif
+;********************************************************************
+		xor	eax,eax
+		ret
+
+_ProcWinMain	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_WinMain	proc
+		local	@stWndClass:WNDCLASSEX
+		local	@stMsg:MSG
+
 		invoke	GetModuleHandle,NULL
 		mov	hInstance,eax
-		;invoke	DialogBoxParam,hInstance,DLG_MAIN,NULL,offset _ProcDlgMain,NULL
-		invoke	ExitProcess,NULL
-		end	start
+		invoke	LoadCursor,hInstance,IDC_MOVE
+		mov	hCursorMove,eax
+		invoke	LoadCursor,hInstance,IDC_MAIN
+		mov	hCursorMain,eax
+;********************************************************************
+; 注册窗口类
+;********************************************************************
+		invoke	RtlZeroMemory,addr @stWndClass,sizeof @stWndClass
+		invoke	LoadIcon,hInstance,ICO_MAIN
+		mov	@stWndClass.hIcon,eax
+		mov	@stWndClass.hIconSm,eax
+		push	hCursorMain
+		pop	@stWndClass.hCursor
+		push	hInstance
+		pop	@stWndClass.hInstance
+		mov	@stWndClass.cbSize,sizeof WNDCLASSEX
+		mov	@stWndClass.style,CS_HREDRAW or CS_VREDRAW
+		mov	@stWndClass.lpfnWndProc,offset _ProcWinMain
+		mov	@stWndClass.hbrBackground,COLOR_WINDOW + 1
+		mov	@stWndClass.lpszClassName,offset szClassName
+		invoke	RegisterClassEx,addr @stWndClass
+;********************************************************************
+; 建立并显示窗口
+;********************************************************************
+		invoke	CreateWindowEx,NULL,\
+			offset szClassName,offset szClassName,\
+			WS_POPUP or WS_SYSMENU,\
+			100,100,WIN_SIZE_X,WIN_SIZE_Y,\
+			NULL,NULL,hInstance,NULL
+		mov	hWinMain,eax
+		invoke	ShowWindow,hWinMain,SW_SHOWNORMAL
+		invoke	UpdateWindow,hWinMain
+;********************************************************************
+; 消息循环
+;********************************************************************
+		.while	TRUE
+			invoke	GetMessage,addr @stMsg,NULL,0,0
+			.break	.if eax	== 0
+			invoke	TranslateMessage,addr @stMsg
+			invoke	DispatchMessage,addr @stMsg
+		.endw
+		ret
+
+_WinMain	endp
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+start:
+		call	_WinMain
+		invoke	ExitProcess,NULL
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		end	start
